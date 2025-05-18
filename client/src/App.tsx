3d25e982baf94f09817d538ee23bd2970d18dcf0
luhvi <luhvilive@gmail.com>
2025-05-18 13:54:55 +0200

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "./App.css";
+import Navbar from "./Navbar";
+
+function App() {
+  return <Navbar />;
+}
+
+export default App;
---
+++ client/src/Navbar.tsx
@@ -0,0 +1,8 @@
+const Navbar = () => {
+  return (
+    <nav className="justify-left fixed top-0 right-0 left-0 flex h-15 items-center bg-gray-800">
+      <h1 className="text-s pl-4 font-medium text-white italic">movies.</h1>
+    </nav>
+  );
+};
+export default Navbar;
